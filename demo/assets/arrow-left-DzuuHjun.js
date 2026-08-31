f6f569a4adcc34520f5e773015438eb154c7ef0e
blaqit CI <ci@blaqit.ru>
2026-08-31 13:18:09 +0300

    chore(Витрина): пересборка из blaqit-crm@f6f248a
    Витрина переехала в основную ветку продукта и собрана из текущего кода.
    Прежняя сборка была от 25.08 и падала на «Фактическом проживании».

---
+++ demo/assets/arrow-left-DzuuHjun.js
@@ -0,0 +1,1 @@
+import{t as o}from"./index-ChDMfYvd.js";const t=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],r=o("arrow-left",t);export{r as A};
--- demo/assets/chevron-right-BNWNnCxl.js
+++
@@ -1,1 +0,0 @@
-import{s as o}from"./index-Bcc26Ufo.js";const t=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],c=o("chevron-right",t);export{c as C};
